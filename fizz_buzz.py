45fb5ffb5539e62cf63e5a5bcc6f735467eb26b9
ChaseCoble <113311559+ChaseCoble@users.noreply.github.com>
2023-05-11 15:35:28 -0500

    Update fizz_buzz.py
    Added dialogue

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,15 +1,53 @@
-import numpy
-import math
-
 def fizz_buzz(max_num, fizz_value, buzz_value):
-    for num in range(1, max_num + 1):
+  fizz_buzz_list = []  
+  for num in range(1, max_num + 1):
         if num % fizz_value == 0 and num % buzz_value == 0:
-            print('FizzBuzz')
+            fizz_buzz_list.append('FizzBuzz')
         elif num % fizz_value == 0:
-            print('Fizz')
+            fizz_buzz_list.append('Fizz')
         elif num % buzz_value == 0:
-            print('Buzz')
+            fizz_buzz_list.append('Buzz')
         else:
-            print(num)
+            fizz_buzz_list.append(num)
+  print(fizz_buzz_list)
+def intCheck(inputVar):
+  isInt = False
+  while not isInt:
+    if inputVar.isdigit():
+      return int(inputVar)
+    else:
+      inputVar = input("That was not a positive integer! Please enter the positive integer you intended: ")
+      
+def inputPrompt(funcVariable):
+  match funcVariable:
+    case "max_num":
+      funcVariable = input("Enter the maximum number of the range you'd like to analyze:  ")
+      return intCheck(funcVariable)
+    case "fizz_value":
+      funcVariable = input("Enter the factor you want replaced with 'Fizz': ")
+      return intCheck(funcVariable)    
+    case "buzz_value": 
+      funcVariable = input("Enter the factor you want replaced with 'Buzz': ")
+      return intCheck(funcVariable)
 
-fizz_buzz(100, 3, 5)
+def continuePrompt():
+    while True:
+        response = input("Would you like to try it again? Type 'y' for yes and 'n' for no: ")
+        if response == "y":
+            return True
+        elif response == "n":
+            return False
+        else:
+            print("That was not a valid answer.")
+
+willContinue = True
+while willContinue:
+  print("This is a function that analyzes a range for two different factors. You will enter a maximum number for a range that starts at 0, and the two factors you'd like to tag. The first factor will be replaced with the word 'Fizz' and the second factor will be replaced with the word 'Buzz'. If the number in the range has both factors, it will be replaced with 'FizzBuzz'. Enjoy!")
+  fizz_buzz(
+    inputPrompt("max_num"),
+    inputPrompt("fizz_value"),
+    inputPrompt("buzz_value")
+  )
+  willContinue = continuePrompt()
+  
+print("Thank you for trying my code!")
